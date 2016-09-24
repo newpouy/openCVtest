4fc034ab338744a91ec8ef6cfa6790c143dcc121
newpouy <newpouy@namee.co.kr>
2016-09-24 16:49:41 +0900

    init

---
+++ openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,121 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/newpouy/Documents/devStudy/openCV_downloaded/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/newpouy/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
